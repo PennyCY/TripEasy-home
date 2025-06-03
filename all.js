0ca0593f3ec4b08b9364b75578135b3c4269e91b
PennyCY <pennylearningmore@gmail.com>
2025-06-03 23:40:27 +0800

    修改旅遊神器互動

--- all.js
+++ all.js
@@ -1,13 +1,13 @@
 //購物專區
-document.querySelector('.hover1').addEventListener('mouseenter', () => {
+document.querySelector('.product-hover1').addEventListener('mouseenter', () => {
   document.querySelector('#shopping').src = 'image-index/shopping/img-shopping view1.png';
 });
 
-document.querySelector('.hover2').addEventListener('mouseenter', () => {
+document.querySelector('.product-hover2').addEventListener('mouseenter', () => {
   document.querySelector('#shopping').src = 'image-index/shopping/img-shopping view2.png';
 });
 
-document.querySelector('.hover3').addEventListener('mouseenter', () => {
+document.querySelector('.product-hover3').addEventListener('mouseenter', () => {
   document.querySelector('#shopping').src = 'image-index/shopping/img-shopping view3.png';
 });
 
@@ -83,5 +83,47 @@ hoverWorld.addEventListener('mouseleave', () => {
   img4.src = R4[index]; 
 });
 
+//旅遊神器
+const buttons = document.querySelectorAll('.weather');
+  const dots = document.querySelectorAll('.dot');
+
+  buttons.forEach((btn, index) => {
+    btn.addEventListener('mouseenter', () => {
+      buttons.forEach(b => b.classList.remove('active'));
+      dots.forEach(d => d.classList.remove('active'));
+      btn.classList.add('active');
+      if (dots[index]) dots[index].classList.add('active');
+    });
+
+    btn.addEventListener('mouseleave', () => {
+      btn.classList.remove('active');
+      if (dots[index]) dots[index].classList.remove('active');
+    });
+  });
+
+  // mouseenter-feature-img
+  document.querySelector('.weather').addEventListener('mouseenter', () => {
+  document.querySelector('#feature').src ='image-index/feature/weather.png';
+  document.querySelector('.feature-right-text').textContent = '隨時查詢即時天氣，提供溫度、降雨與風速資訊，讓您安心出門';
+});
+
+document.querySelector('.rate').addEventListener('mouseenter', () => {
+  document.querySelector('#feature').src = 'image-index/feature/rate.png';
+  document.querySelector('.feature-right-text').textContent = '快速換算全球貨幣，提供最新匯率，讓旅行與購物更便利';
+
+});
+
+document.querySelector('.hotel').addEventListener('mouseenter', () => {
+  document.querySelector('#feature').src = 'image-index/feature/hotel.png';
+  document.querySelector('.feature-right-text').textContent = '即時比價全球航班，輕鬆找到最划算機票';
+});
+document.querySelector('.maps').addEventListener('mouseenter', () => {
+  document.querySelector('#feature').src = 'image-index/feature/Ai-map.png';
+  document.querySelector('.feature-right-text').textContent = '根據您的定位，提供真實度圖畫面，為您指引正確道路';
+});
+document.querySelector('.culture').addEventListener('mouseenter', () => {
+  document.querySelector('#feature').src = 'image-index/feature/culture.png';
+  document.querySelector('.feature-right-text').textContent = '根據您的所在位置，提供當地文化與特色地標等介紹';
+});
 // 初始化
 startAutoSwitch();
