eea2c98838e2460d2a55239b37a257d8968dfb12
PennyCY <pennylearningmore@gmail.com>
2025-06-11 19:46:30 +0800

    新增客服FAQ

--- all.js
+++ all.js
@@ -127,3 +127,28 @@ document.querySelector('.culture').addEventListener('mouseenter', () => {
 });
 // 初始化
 startAutoSwitch();
+
+
+
+// //testtttt
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   // direction: 'vertical',
+//   // loop: true,
+
+//   // If we need pagination
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+// //   scrollbar: {
+// //     el: '.swiper-scrollbar',
+// //   },
+// });
